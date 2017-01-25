e0888921943f72d7c77900fa66fac972bafff74a
Giofanny Mowoka <giofanny.mowoka@gmail.com>
2017-01-26 00:17:35 +0700

    edit luas lingkaran

--- tugas1/HitungLuas.java
+++ tugas1/HitungLuas.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class HitungLuas {
     public static void main(String []args){
         int p = 10;
         int l = 8;
         int luas = p*l;
+        double luas2;
+        int jarijari;
         System.out.print("Luas Persegi panjang bila diketahui Panjang(10 cm) dan Lebar (8 cm) adalah "+luas+" cm");
+  	 Scanner scan = new Scanner(System.in);
+   System.out.println("Menghitung Luas Lingkaran");
+   System.out.print("Masukan Jari-Jari: ");
+   jarijari = scan.nextInt();
+ 
+   luas2 = 3.14 * (jarijari * jarijari);
+   System.out.println("Luas Lingkaran= " +luas2); 
+
     }
 }
