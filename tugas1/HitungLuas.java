9634d65ceef8e3db0f66891301296683cc7ed78f
Giofanny Mowoka <giofanny.mowoka@gmail.com>
2017-01-26 00:10:04 +0700

    ver Hitung luas Persegipanjang

---
+++ tugas1/HitungLuas.java
@@ -0,0 +1,8 @@
+public class HitungLuas {
+    public static void main(String []args){
+        int p = 10;
+        int l = 8;
+        int luas = p*l;
+        System.out.print("Luas Persegi panjang bila diketahui Panjang(10 cm) dan Lebar (8 cm) adalah "+luas+" cm");
+    }
+}
