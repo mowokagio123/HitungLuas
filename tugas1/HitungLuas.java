d80f197522ef411f828f86240c7b7a539ccb5fa7
Giofanny Mowoka <giofanny.mowoka@gmail.com>
2017-01-26 00:22:32 +0700

    ver Hitung Segitiga

--- tugas1/HitungLuas.java
+++ tugas1/HitungLuas.java
@@ -3,6 +3,10 @@ public class HitungLuas {
         int p = 10;
         int l = 8;
         int luas = p*l;
-        System.out.print("Luas Persegi panjang bila diketahui Panjang(10 cm) dan Lebar (8 cm) adalah "+luas+" cm");
+        int a = 7;
+        int t = 5;
+        double luas2 = 0.5*a*t;
+        System.out.println("Luas Persegi panjang bila diketahui Panjang(10 cm) dan Lebar (8 cm) adalah "+luas+" cm");
+ 	 System.out.println("Luas Segitiga bila alas (7) dan tinggi (5) adalah "+luas2 );
     }
 }
